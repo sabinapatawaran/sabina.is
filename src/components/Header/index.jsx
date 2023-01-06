d4f6099bfedb374a6c3fe789e9df7ffd9eecbf0c
Sabina Patawaran <sabinapatawaran@Sabinas-MacBook-Pro.local>
2023-01-06 17:03:25 +1100

    useLocation hook imported and functioning

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+
 const Header = () => {
+    const location = useLocation();
+    console.log(location);
+
     return (
         <header>
             <h1>sabina.is/</h1>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <main>
             <div>
-                <h1>sabina.is/a-gen-z-woman-of-colour-trying-to-do-the-write-thing</h1>
+                <h2>sabina.is/a-gen-z-woman-of-colour-trying-to-do-the-write-thing</h2>
             </div>
         </main>
     );

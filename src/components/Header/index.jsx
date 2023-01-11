d4c4de965f6ea24a43f3b81eb3c5cf986f39f5ed
Sabina Patawaran <sabinapatawaran@Sabinas-MacBook-Pro.local>
2023-01-12 09:22:43 +1100

    suitable typing animation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,24 @@
 import { useLocation } from "react-router-dom";
 import styles from "./Header.module.css";
+import Typed from 'react-typed';
 
 // Option: put typewriter component, put in the strings. And THEN apply the header styling to the whole header tag WRAPPING our typewriter.
+// Add use effect wherein clicking another link triggers a "delete" scroll effect.
 
 const Header = () => {
     const { pathname } = useLocation();
 
-    const headerStyles = pathname==="/" ? `${styles["welcome-header"]} ${styles["typed-fade-out"]}` : `${styles["other-header"]} ${styles["typed-fade-out"]}`
+    // const headerStyles = pathname==="/" ? `${styles["welcome-header"]} ${styles["typed-fade-out"]}` : `${styles["other-header"]} ${styles["typed-fade-out"]}`
 
     return (
         <header>
-
+            <h1>
+                sabina.is/
+                <Typed
+                    strings={[`trying-to-do-the-write-thing`]}
+                    typeSpeed={20}
+                    backSpeed={20} />
+            </h1>
             {/* <h1 className={headerStyles}>sabina.is/
                 {pathname === "/" && "a-gen-z-woman-of-colour-trying-to-do-the-write-thing"}
                 {pathname === "/about" && "someone-you-should-know-about"}
@@ -23,3 +31,5 @@ const Header = () => {
 }
 
 export default Header;
+
+// string interpolation -> colour the 'write'

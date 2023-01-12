d7857ab20b79f861d611cc4c4558e875b250aea4
Sabina Patawaran <sabinapatawaran@Sabinas-MacBook-Pro.local>
2023-01-12 14:36:09 +1100

    about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,15 @@
+import Headshot from "../assets/film-photo.JPG";
+
 const About = () => {
     return (
-        <section>
-            <h1>About</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor vitae delectus id voluptatum nam repellendus provident non magnam fugit reprehenderit sunt saepe doloremque veritatis eveniet, suscipit, commodi quisquam! Quam, voluptate.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolores, molestias adipisci nobis dignissimos obcaecati repudiandae exercitationem deleniti molestiae, accusamus sequi autem perspiciatis iste. Minima, aperiam et. Placeat, facilis vero.</p>
+        <section className="nester">
+            <img className="profile" src={Headshot} alt="Headshot of myself"/>
+            <section className="subnest">
+                <p>Sabina Patawaran (she/they) is a Gen Z woman of colour, trying to do the [write] thing. Based on Dharug Ngurra (Sydney, Australia), she is an undergraduate at the University of Sydney, completing the Advanced Economics program and double majoring in economics and international relations.</p>
+                <p>Sponsored by her university, she is completing a policy fellowship under Global Voices, and will be attending the UN Commission on the Status of Women 67 in New York in 2023. She is completing a policy paper on blockchain regulation and the financial wellness of immigrant women of colour.</p>
+                <p>In 2020, she co-founded the <a href="www.antiracismkit.com.au" target="_blank">Anti-Racism Kit</a> with Jinyoung Kim. She is currently developing the latest iteration of this resource, which is set to be published under Hardie Grant next summer. She is also an aspiring front-end developer.</p>
+                <p>Much to her younger self's ire, it seems she <i>has</i> become a #WomanInSTEM. Go figure.</p>
+            </section>
         </section>
     );
 };
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,7 +2,7 @@ import LinkedIn from "../assets/linkedin-logo.png";
 
 const Contact = () => {
     return (
-        <section>
+        <section className="standard-section">
             <p>Say <a href="mailto:hello@sabina.is?Subject=Hey" target="_top">hello@sabina.is</a> (Well, write, I suppose, since it'll be an email.)</p>
             <p>Or perhaps you're more <a href="https://www.linkedin.com/in/sabinapatawaran/"><img style={{ width: `1rem`, height: `1rem` }} src={LinkedIn} alt="LinkedIn logo"/>clined to social media.</a></p>
         </section>
